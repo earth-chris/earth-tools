ddf9c35aeb58267f2857418b4ca4cadd333f0bae
christobal54 <cbanderson08@gmail.com>
2015-09-03 23:17:51 -0700

    added more command line options

--- aeilas.py
+++ aeilas.py
@@ -18,6 +18,7 @@ params = {
     'max_tch' : 65, # max height above which to clip spurious (noise) points
     'outdir' : aei.get_outdir(), # the output directory for final products
     'outfile' : 'default', # the default output basename
+    'overwrite' : False, # default to not overwrite any existing files
     'procdir' : aei.get_procdir(), # the output directory for scratch files
     'res' : 1, # default raster resolution
     'slicer_res' : 5, # slicer resolution (usually 5x raster res)
@@ -34,7 +35,7 @@ def usage():
     print(
         """
         $ aeilas.py [-cores cores] [-lt las_type][-max_tch max_tch] 
-        [-o output_file] [-proj proj4string]
+        [-o output_file] [-overwrite] [-proj proj4string]
         [-res resolution] [-sres slicer_resolution] [-tile_size tile_size] 
         input_files
         
@@ -43,6 +44,21 @@ def usage():
         )
         
     sys.exit(1)
+
+step = 0
+def resume():
+    """
+    resumes the aeilas.py procedure when flagged at runtime
+    
+    syntax: resume()
+    """
+    print(steps)
+    while True:
+        try:
+            step=int(raw_input('Select the step to resume: '))
+            break
+        except ValueError:
+            print('Invalid entry. Please enter number from 1 to %d' % (len(steps)))
         
 def main ():
     """
@@ -59,18 +75,36 @@ def main ():
     if arg.lower() == '-cores':
         i += 1
         arg = sys.argv[i]
-        
-        params['cores'] = arg
-        
-    if arg.lower() == '-lt':
-        i += 1
-        arg = sys.argv[i]
         try:
             if arg < multiprocessing.cpu_count():
                 params['cores'] = arg
         except ValueError:
             print("Could not parse -cores argument: %s" % (arg))
             usage()
+        
+    if arg.lower() == '-lt':
+        i += 1
+        arg = sys.argv[i]
+        try:
+            if (arg.lower() != 'las') or (arg.lower() != 'laz'):
+                params['lt'] = arg
+        except ValueError:
+            print("Could not parse -lt argument: %s" % (arg))
+            usage()
+            
+    if arg.lower() == '-max_tch':
+        i += 1
+        arg = sys.argv[i]
+        try:
+            if arg > 0:
+                params['max_tch'] = arg
+        except ValueError:
+            print("Could not parse -max_tch argument: %s" % (arg))
+            usage()
+            
+    if arg.lower() == '-o':
+        i += 1
+        arg = sys.argv[i]
         
 if __name__ == "__main__":
     main()
