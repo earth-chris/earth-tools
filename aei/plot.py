43fec71d6daf1da195c5923a1f0b7d270524001d
Christopher Anderson <cbanderson08@gmail.com>
2017-07-06 16:33:42 -0700

    first commit, not much done

---
+++ aei/plot.py
@@ -0,0 +1,16 @@
+#####
+# functions to plot some plotty plots
+#
+# c. 2016-2017 Christopher Anderson
+#####
+
+# function to plot density distributions
+def density_dist(ydata, xlim = None, ylim = None, **kwargs):
+    """
+    """
+    import numpy
+    import matplotlib.pyplot as plt
+    from scipy.stats import gaussian_kde
+    
+    # uh, deal with this later
+    return None
