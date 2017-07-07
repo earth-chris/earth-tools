d12c99cb9bd8c853de7a1ced72c2186bf972e753
christobal54 <cbanderson08@gmail.com>
2017-07-06 23:14:01 -0700

    updated custom color maps to use new color object

--- aei/color.py
+++ aei/color.py
@@ -8,23 +8,26 @@ def rgb_to_hex(rgb):
     """Return color as #rrggbb for the given color values."""
     return '#%02x%02x%02x' % (rgb[0], rgb[1], rgb[2])
 
-def color_blind(n = None, alpha = None):
+def color_blind(n = None, alpha = None, black = False):
     """Returns a color blind friendly color palette
     """
     from aei import objects
-    base = ["#E69F00", "#009E73", "#0072B2", "#D55E00", "#F0E442", 
+    
+    # set metadata
+    name = 'color_blind'
+    palette = ["#E69F00", "#009E73", "#0072B2", "#D55E00", "#F0E442", 
         "#56B4E9", "#CC79A7", "#000000"]
+        
+    # remove the black value if set
+    if not black:
+        palette.remove("#000000")
     
     # create the baseline object
-    cp = objects.color(base)
-    
-    # interploate the colors if set
-    if n:
-        cp.interpolate(n)
+    cp = objects.color(palette = palette, n = n, name = name)
         
     # add alpha values if set
     if alpha:
-        cp.add_alpha(alpha)
+        cp.alpha(alpha)
         
     return cp.palette
     
@@ -32,18 +35,17 @@ def stanford(n = None, alpha = None):
     """Returns the stanford base color palette
     """
     from aei import objects
-    base = ["#8C1515", "#4D4F53", "#000000"]
+    
+    # set metadata
+    name = 'stanford_primary'
+    palette = ["#8C1515", "#4D4F53", "#000000"]
     
     # create the baseline object
-    cp = objects.color(base)
+    cp = objects.color(palette = palette, n = n, name = name)
     
-    # interploate the colors if set
-    if n:
-        cp.interpolate(n)
-        
     # add alpha values if set
     if alpha:
-        cp.add_alpha(alpha)
+        cp.alpha(alpha)
         
     return cp.palette
         
@@ -51,18 +53,17 @@ def stanford_bright(n = None, alpha = None):
     """Returns the stanford bright color palette
     """
     from aei import objects
-    base = ["#0098DB", "#007C92", "#009B76", "#B26F16", "#E98300", "#EAAB00"]
+    
+    # set metadata
+    name = 'stanford_bright'
+    palette = ["#0098DB", "#007C92", "#009B76", "#B26F16", "#E98300", "#EAAB00"]
     
     # create the baseline object
-    cp = objects.color(base)
+    cp = objects.color(palette = palette, n = n, name = name)
     
-    # interploate the colors if set
-    if n:
-        cp.interpolate(n)
-        
     # add alpha values if set
     if alpha:
-        cp.add_alpha(alpha)
+        cp.alpha(alpha)
         
     return cp.palette
         
@@ -70,18 +71,17 @@ def stanford_dark(n = None, alpha = None):
     """Returns the stanford dark color palette
     """
     from aei import objects
-    base = ["#5E3032", "#8D3C1E", "#00505C", "#175E54", "53284F"]
+    
+    # set metadata
+    name = 'stanford_dark'
+    palette = ["#5E3032", "#8D3C1E", "#00505C", "#175E54", "#53284F"]
     
     # create the baseline object
-    cp = objects.color(base)
+    cp = objects.color(palette = palette, n = n, name = name)
     
-    # interploate the colors if set
-    if n:
-        cp.interpolate(n)
-        
     # add alpha values if set
     if alpha:
-        cp.add_alpha(alpha)
+        cp.alpha(alpha)
         
     return cp.palette
         
@@ -89,18 +89,17 @@ def stanford_neutral(n = None, alpha = None):
     """Returns the stanford neutral color palette
     """
     from aei import objects
-    base = ["#D2C295", "#B3995D", "#928B81", "#B6B1A9", 
+    
+    # set metadata
+    name = 'stanford_neutral'
+    palette = ["#D2C295", "#B3995D", "#928B81", "#B6B1A9", 
         "#DAD7CB", "#C7D1C5", "#4D5357", "#5F574F"]
     
     # create the baseline object
-    cp = objects.color(base)
+    cp = objects.color(palette = palette, n = n, name = name)
     
-    # interploate the colors if set
-    if n:
-        cp.interpolate(n)
-        
     # add alpha values if set
     if alpha:
-        cp.add_alpha(alpha)
+        cp.alpha(alpha)
         
     return cp.palette
