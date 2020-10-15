d4db17693405091797b4e0c1fa0eceaa37b3cc0d
earth-chris <cbanderson08@gmail.com>
2020-10-15 11:22:06 -0700

    proper install

--- setup.py
+++ setup.py
@@ -1,18 +1,29 @@
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup
 
-config = {
-    'description': 'personal image processing and ecological analysis tools',
-    'author': 'Christopher B. Anderson',
-    'url': 'https://earth-chris.github.io',
-    'download_url': 'https://github.com/christobal54/earth-tools.git',
-    'author_email': 'cbanders@stanford.edu',
-    'version': '0.1',
-    'install_requires': ['gdal', 'ogr', 'osr', 'numpy', 'spectral', 'psutil'],
-    'packages': ['earthtools','bin'],
-    'name': 'earthtools'
+version = open("earthtools/__version__.py").read().strip('"\n')
+
+setup_args = {
+    "name": "earthtools",
+    "version": version,
+    "url": "https://earth-chris.github.io",
+    "license": "MIT",
+    "author": "Christopher Anderson",
+    "author_email": "cbanders@stanford.edu",
+    "description": "Personal tools for image processing and ecological analysis",
+    "keywords": ["biogeography", "SDM", "species distribution modeling", "ecologyy", "conservation"],
+    "packages": ["earthtools"],
+    "include_package_data": True,
+    "platforms": "any",
+    "scripts": [
+        "bin/et-align.py",
+        "bin/et-las.py",
+        "bin/et-mask.py",
+        "bin/et-moment.py",
+        "bin/et-palsar.py",
+        "bin/et-planet.py",
+        "bin/et-sample-raster.py",
+        "bin/et-unmix.py",
+    ],
 }
 
-setup(**config)
+setup(**setup_args)
